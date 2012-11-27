1d91092fe6318a887ee7b407c8ac62a1b3ec2b4a
Joel Pelaez Jorge <joelpelaez@gmail.com>
2012-11-27 07:32:41 -0600

    Add license header

--- src/org/darksoft/android/multimedia/remotecontrol/BluetoothClient.java
+++ src/org/darksoft/android/multimedia/remotecontrol/BluetoothClient.java
@@ -1,3 +1,22 @@
+/*
+ *  This file is part of Multimedia Remote Control
+ *  BluetoothClient.java - Bluetooth client connection interface.
+ *  Copyright (C) 2012  Joel Peláez Jorge <joelpelaez@gmail.com>
+ * 
+ *  Multimedia Remote Control is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Multimedia Remote Control is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Multimedia Remote Control.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.darksoft.android.multimedia.remotecontrol;
 
 import java.io.IOException;
